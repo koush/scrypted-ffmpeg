930b309f833395889fe3956025818762a3e5bd70
Koushik Dutta <koush@koushikdutta.com>
2021-12-30 10:59:39 -0800

    update

--- build-windows.sh
+++ build-windows.sh
@@ -1,11 +1,1 @@
-OUTPUTDIR=/output
-
 ./cross_compile_ffmpeg.sh --build-ffmpeg-shared=y --build-ffmpeg-static=y --disable-nonfree=n --build-intel-qsv=y --compiler-flavors=win64 --enable-gpl=y
-
-mkdir -p $OUTPUTDIR/static/bin
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffmpeg.exe $OUTPUTDIR/static/bin
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffprobe.exe $OUTPUTDIR/static/bin
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffplay.exe $OUTPUTDIR/static/bin
-
-mkdir -p $OUTPUTDIR/shared
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_shared/bin/ $OUTPUTDIR/shared
