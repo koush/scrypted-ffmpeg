13f4407f71ad34fcc3662939cedc24c5eaac3f21
Koushik Dutta <koush@koushikdutta.com>
2022-01-10 22:30:06 -0800

    windows

--- copy-outputs-windows.sh
+++ copy-outputs-windows.sh
@@ -1,5 +1,10 @@
 cd $(dirname $0)
 
-# docker pull koush/ffmpeg-build:windows
+
+if [ ! -z "$DO_BUILD" ]
+then
+    docker build -t koush/ffmpeg-build:windows -f Dockerfile.windows .
+fi
+
 OUTPUT=$PWD/outputs
 docker run -v $OUTPUT:/docker-output -it --entrypoint /docker-output/copy-docker-output-windows.sh koush/ffmpeg-build:windows
